707cfd51f2bcaebf93a6c6daa0cd4c8776f118dd
montaj <sekhmontajulali1997@gmail.com>
2023-07-06 20:04:02 +0530

    fbtimeline

---
+++ app.js/data.js
@@ -0,0 +1,147 @@
+const fbPost =[
+    {
+        id:1,
+        autor: {
+            name: 'Dr. Jahangir kabir',
+            au_photo: 'https://scontent.fblr4-2.fna.fbcdn.net/v/t39.30808-6/357584174_796729665235762_2453697540642305581_n.jpg?stp=cp6_dst-jpg&_nc_cat=1&cb=99be929b-59f725be&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=A7WJA9QW7_kAX8c8w6v&_nc_ht=scontent.fblr4-2.fna&oh=00_AfBT1BW3Us4VIaG3l0wJKnNFJ5OO3TyGi52RXQy8JBURVg&oe=64A676BE',
+            post_time: '9 hours ago',
+        },
+        p_title: 'Relaxing Sun bathing at Sea Beach',
+        p_photo:'https://scontent.fblr4-2.fna.fbcdn.net/v/t39.30808-6/357584174_796729665235762_2453697540642305581_n.jpg?stp=cp6_dst-jpg&_nc_cat=1&cb=99be929b-59f725be&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=A7WJA9QW7_kAX8c8w6v&_nc_ht=scontent.fblr4-2.fna&oh=00_AfBT1BW3Us4VIaG3l0wJKnNFJ5OO3TyGi52RXQy8JBURVg&oe=64A676BE',
+        p_like: '8k',
+        p_comment: 306,
+        
+
+    },
+
+    {
+        id:2,
+        autor: {
+            name: 'Arpan Raj Sanyal',
+            au_photo: 'https://scontent.fblr4-3.fna.fbcdn.net/v/t1.6435-1/209046213_1496213727388634_1912185763652239538_n.jpg?stp=cp0_dst-jpg_p60x60&_nc_cat=109&cb=99be929b-59f725be&ccb=1-7&_nc_sid=7206a8&_nc_ohc=Y-hyRN3uKykAX9iGntU&_nc_ht=scontent.fblr4-3.fna&oh=00_AfBULjZ5--u4uLXc24ofpnkcnFBPoRte_kklgFedAWmvpA&oe=64C9041B',
+            post_time: 'a day ago',
+        },
+        p_title: 'Basudev Sanyal biriyani valo khate lage 😂😂😂😂',
+        p_photo:'https://scontent.fblr4-2.fna.fbcdn.net/v/t39.30808-6/356387378_834561704935624_3956275928277809408_n.jpg?stp=dst-jpg_s960x960&_nc_cat=1&cb=99be929b-59f725be&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=hcUll5XGKMQAX_0JTXW&_nc_ht=scontent.fblr4-2.fna&oh=00_AfCThOTif8L4nlVNgc8cAW7-oRymwLEkWJ0yvXk-ngewdQ&oe=64A60338',
+        p_like: '0',
+        p_comment: 0,
+        
+
+    },
+
+    {
+        id:3,
+        autor: {
+            name: 'নারী',
+            au_photo: 'https://scontent.fblr4-2.fna.fbcdn.net/v/t39.30808-1/294614458_165066532702316_8709178063725116873_n.jpg?stp=dst-jpg_s320x320&_nc_cat=1&cb=99be929b-59f725be&ccb=1-7&_nc_sid=c6021c&_nc_ohc=ZICdDSw4d2MAX9kigaF&_nc_ht=scontent.fblr4-2.fna&oh=00_AfAMSM-dds_6mEtTyzAwFGbzlL2IhJyhhccOk_XQ0_LJPA&oe=64A5E73E',
+            post_time: 'a day ago',
+        },
+        p_title: 'লিজেন্ডরা বুঝবে বাকিরা খুজবে 😎😎',
+        p_photo:'https://scontent.fblr4-2.fna.fbcdn.net/v/t39.30808-6/349902984_281974354344866_3863675940349648955_n.jpg?stp=dst-jpg_p843x403&_nc_cat=1&cb=99be929b-59f725be&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=DG-lEYsbaxUAX91of8w&_nc_ht=scontent.fblr4-2.fna&oh=00_AfBtk4Isi1v4umAVrb0lbgh3L1UNkHCAST1hKY0CVA6hCw&oe=64A5F707',
+        p_like: '72k',
+        p_comment: '6.5k',
+        
+
+    },
+    {
+        id:4,
+        autor: {
+            name: 'ইলমে রাহি শেফা',
+            au_photo: 'https://scontent.fblr4-2.fna.fbcdn.net/v/t39.30808-1/356899289_2140157986188514_7458414003442911886_n.jpg?stp=cp0_dst-jpg_p60x60&_nc_cat=1&cb=99be929b-59f725be&ccb=1-7&_nc_sid=f67be1&_nc_ohc=-0_565VU_tYAX8-MEm6&_nc_ht=scontent.fblr4-2.fna&oh=00_AfBPXeK8WjgH8ClUX1Q6wPXpSFwWwfGXfeGR9QhcxEY3zA&oe=64A5A757',
+            post_time: '3 days ago',
+        },
+        p_title: 'অথচ কিছুই করলাম না জীবনে 😕',
+        p_photo:'https://scontent.fblr4-2.fna.fbcdn.net/v/t39.30808-6/356680370_2138409079696738_4350542386577538652_n.jpg?_nc_cat=1&cb=99be929b-59f725be&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=0oGgbUaSeigAX_kaKLI&_nc_ht=scontent.fblr4-2.fna&oh=00_AfAI0a4t06l9l3vvYyGAAWdHvIcwcXp8g4Ldmhbk0-pjiw&oe=64A721C8',
+        p_like: '202k',
+        p_comment: '3.5k',
+        
+
+    },
+    {
+        id:5,
+        autor: {
+            name: 'Shamayeera Yeasmin',
+            au_photo: 'https://scontent.fblr4-2.fna.fbcdn.net/v/t39.30808-1/307760689_2994049070895231_6040327311591134193_n.jpg?stp=c0.0.60.60a_cp0_dst-jpg_p60x60&_nc_cat=105&cb=99be929b-59f725be&ccb=1-7&_nc_sid=7206a8&_nc_ohc=GIWAYrsJXd0AX9OFakw&_nc_ht=scontent.fblr4-2.fna&oh=00_AfCddEKaUMk0So7altxmi_pta25BdhD77kn893BT2TXLTg&oe=64A59D01',
+            post_time: 'a days ago',
+        },
+        p_title: 'দুরন্ত ঘূর্ণির ওই লেগেছে পাক 💫',
+        p_photo:'https://scontent.fblr4-4.fna.fbcdn.net/v/t39.30808-6/356999928_1482224365645824_5736200144492144421_n.jpg?stp=cp6_dst-jpg_p843x403&_nc_cat=104&cb=99be929b-59f725be&ccb=1-7&_nc_sid=0debeb&_nc_ohc=I8AVMwGw_u8AX8TCXFr&_nc_ht=scontent.fblr4-4.fna&oh=00_AfBnBBFxTqkpdT5X7Exu2KEkE5ZBEnCqDhrNVCoKJwColA&oe=64A5ABD8',
+        p_like: '7k',
+        p_comment: '3.5k',
+        
+
+    },
+    {
+        id:6,
+        autor: {
+            name: 'Spinny',
+            au_photo: 'https://scontent.fblr4-3.fna.fbcdn.net/v/t39.30808-1/338190425_932583048197105_9161056165656798967_n.jpg?stp=cp0_dst-jpg_p60x60&_nc_cat=1&cb=99be929b-59f725be&ccb=1-7&_nc_sid=c6021c&_nc_ohc=rR3qin0XEHYAX87CuiA&_nc_ht=scontent.fblr4-3.fna&oh=00_AfCZWOwZXNSKrN8GeElf1n2GsNALmynI9abY8F8NZ97VQw&oe=64ABE84A',
+            post_time: 'a days ago',
+        },
+        p_title: 'Sell your Used car instantly',
+        p_photo:'https://scontent.fblr4-3.fna.fbcdn.net/v/t45.1600-4/307819314_23851575408060258_1570452272997783015_n.jpg?stp=c0.132.526.526a_cp0_dst-jpg_p526x296_q75_spS444&_nc_cat=1&cb=99be929b-59f725be&ccb=1-7&_nc_sid=e84a38&_nc_ohc=wWk4WjXZGy4AX9kx_8O&_nc_ht=scontent.fblr4-3.fna&oh=00_AfA5exR4UdhTMLucvJR24LPcK8Ywgn20mwUJHwQO25dgFQ&oe=64AB0631',
+        p_like: '4k',
+        p_comment: '1.5k',
+        
+
+    },
+    {
+        id:7,
+        autor: {
+            name: 'অভিমান',
+            au_photo: 'https://scontent.fblr4-3.fna.fbcdn.net/v/t39.30808-1/293172325_581278043366532_9146259199098053541_n.jpg?stp=c0.0.60.60a_cp0_dst-jpg_p60x60&_nc_cat=110&cb=99be929b-59f725be&ccb=1-7&_nc_sid=c6021c&_nc_ohc=PIY2fujZvBwAX9WmYpY&_nc_ht=scontent.fblr4-3.fna&oh=00_AfBJ46ziLo7la6K8PiuNGvh_x5XeES2nOK2Z3aROG6VYdg&oe=64AA67EA',
+            post_time: '11 hours ago',
+        },
+        p_title: 'হ্যাঁ 🖤',
+        p_photo:'https://scontent.fblr4-2.fna.fbcdn.net/v/t39.30808-6/357584086_813168810177453_3005124394763112984_n.jpg?_nc_cat=105&cb=99be929b-59f725be&ccb=1-7&_nc_sid=730e14&_nc_ohc=XrTMprZwV0wAX-W0tp5&_nc_ht=scontent.fblr4-2.fna&oh=00_AfClQQfw7Y8FtdIM6yyqT4fpTCbwgEC5Yli52YzLJsjaUQ&oe=64AB0978',
+        p_like: '1k',
+        p_comment: '500',
+        
+
+    },
+    {
+        id:8,
+        autor: {
+            name: 'Kalachan Miya',
+            au_photo: 'https://scontent.fblr4-3.fna.fbcdn.net/v/t39.30808-1/329153653_3402859886637014_1463135864061964019_n.jpg?stp=cp0_dst-jpg_p60x60&_nc_cat=1&cb=99be929b-59f725be&ccb=1-7&_nc_sid=c6021c&_nc_ohc=r95iGjretvIAX-fO0Ge&_nc_ht=scontent.fblr4-3.fna&oh=00_AfDr5Cg1OXLowNRuBdsbTscALIo-GAvT8qAx58zoHIwkIQ&oe=64AA9D86',
+            post_time: '19 hours ago',
+        },
+        p_title: 'বয়ফেরেনের বিয়াতে প্রাক্তনরা তো আর ডিরেক্ট আইতে পারে না😷তাই এই রুপ ধারন কইরা আইসে পোতিশুদ নিতে🤣🤣🤣',
+        p_photo:'https://www.facebook.com/reel/583081057335663',
+        p_like: '4k',
+        p_comment: '2.5k',
+        
+
+    },
+    {
+        id:9,
+        autor: {
+            name: 'Āfțâb Vaıjan',
+            au_photo: 'https://scontent.fblr4-5.fna.fbcdn.net/v/t39.30808-1/343459977_133921366327796_6360614180117796593_n.jpg?stp=cp0_dst-jpg_p60x60&_nc_cat=103&cb=99be929b-59f725be&ccb=1-7&_nc_sid=f67be1&_nc_ohc=UALN4iTw8S0AX-AZXLU&_nc_ht=scontent.fblr4-5.fna&oh=00_AfAK00neLOmqNsWj-ZpeW6j1NGS0P08JObNqk4ZHKbgB5w&oe=64AA7A60',
+            post_time: 'a days ago',
+        },
+        p_title: 'Digital India',
+        p_photo:'https://fb.watch/lB_BQkCFEG/',
+        p_like: '1k',
+        p_comment: '100',
+        
+
+    },
+    {
+        id:10,
+        autor: {
+            name: 'Noya Jamai',
+            au_photo: 'https://scontent.fblr4-3.fna.fbcdn.net/v/t39.30808-1/346786369_1009317730051221_9126147989720169622_n.jpg?stp=cp0_dst-jpg_p60x60&_nc_cat=1&cb=99be929b-59f725be&ccb=1-7&_nc_sid=1eb0c7&_nc_ohc=YKvJ2D_MzJAAX9r83WQ&_nc_ht=scontent.fblr4-3.fna&oh=00_AfCN4Fu94RWKg3MQC6uI74c32W79X29LiAnBPxHLv8OgQA&oe=64AB7B00',
+            post_time: 'a days ago',
+        },
+        p_title: 'গরীব😜',
+        p_photo:'https://scontent.fblr4-3.fna.fbcdn.net/v/t39.30808-6/352374123_261776279881697_8677643599607914809_n.jpg?stp=dst-jpg_p843x403&_nc_cat=1&cb=99be929b-59f725be&ccb=1-7&_nc_sid=8bfeb9&_nc_ohc=_rYFYWHFeNMAX9Osa3a&_nc_ht=scontent.fblr4-3.fna&oh=00_AfB9NnCH3DFjn60g4qp6eQjNZR9DQzg3jP8uLpcqSCYPsw&oe=64AB2859',
+        p_like: '1k',
+        p_comment: '200',
+        
+
+    },
+
+
+
+];
